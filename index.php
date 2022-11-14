8592a8b9d3ad2bb02832210824335f31b29537a6
Xenophmix <xenophmix@gmail.com>
2022-11-14 10:32:24 +0800

    連線資料庫

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,20 +8,29 @@
     <title>學生管理系統</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
     <h1>學生管理系統</h1>
-<?php
-$dsn="mysql:host=localhost;charset=utf8;dbname=school";
-$pdo=new PDO($dsn,'root','');
+    <?php
+    // $dsn="mysql:host=localhost;charset=utf8;dbname=school";
+    $db = mysqli_connect('localhost', 'root','','school');
+    mysqli_set_charset($db,'utf8');
 
-$sql="SELECT * FROM `students` LIMIT 5";
+    $sql = "SELECT * FROM `students` LIMIT 5";
+    $result = mysqli_query($db,$sql);
+    $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    
 
-$rows=$pdo -> query($sql) -> fetchAll(PDO::FETCH_ASSOC);
 
-echo "<pre>";
-print_r($rows);
-echo "</pre>";
 
-?>
+    // $rows = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    // $pdo = new PDO($dsn, 'root', '');
+    
+    echo "<pre>";
+    print_r($rows);
+    echo "</pre>";
+
+    ?>
 </body>
+
 </html>
