e3098b60fbb37de75972dbdd9e124f2ed1e68632
Xenophmix <xenophmix@gmail.com>
2022-11-14 11:19:43 +0800

    計算年齡，及美化

--- index.php
+++ index.php
@@ -8,29 +8,58 @@
     <title>學生管理系統</title>
     <link rel="stylesheet" href="style.css">
 </head>
+<?php
+// $dsn="mysql:host=localhost;charset=utf8;dbname=school";
+$db = mysqli_connect('localhost', 'root', '', 'school');
+mysqli_set_charset($db, 'utf8');
+
+$sql = "SELECT * FROM `students` LIMIT 20";
+$result = mysqli_query($db, $sql);
+$rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+
+
+
+// $rows = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+// $pdo = new PDO($dsn, 'root', '');
+
+// echo "<pre>";
+// print_r($rows);
+// echo "</pre>";
+
+?>
 
 <body>
     <h1>學生管理系統</h1>
-    <?php
-    // $dsn="mysql:host=localhost;charset=utf8;dbname=school";
-    $db = mysqli_connect('localhost', 'root','','school');
-    mysqli_set_charset($db,'utf8');
 
-    $sql = "SELECT * FROM `students` LIMIT 5";
-    $result = mysqli_query($db,$sql);
-    $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
-    
+    <table class="list-students">
+
+        <tr">
+            <td>學號</td>
+            <td>姓名</td>
+            <td>身份證字號</td>
+            <td>生日</td>
+            <td>畢業國中</td>
+            <td>年齡</td>
+        </tr>
+        <?php
+        foreach ($rows as $row) {
+
+            $age = floor((strtotime('now') - strtotime($row['birthday'])) / (60 * 60 * 24 * 365));
+
+            echo "<tr>";
+            echo "<td>{$row['school_num']}&nbsp;</td>";
+            echo "<td>{$row['name']}</td>";
+            echo "<td>{$row['uni_id']}</td>";
+            echo "<td>{$row['birthday']}</td>";
+            echo "<td>{$row['graduate_at']}</td>";
+            echo "<td>&nbsp$age</td>";
+            echo "</tr>";
+        }
+        ?>
+    </table>
 
 
-
-    // $rows = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
-    // $pdo = new PDO($dsn, 'root', '');
-    
-    echo "<pre>";
-    print_r($rows);
-    echo "</pre>";
-
-    ?>
 </body>
 
 </html>
