bde690210a0a3caa22ff76e5272b44ebc40980b1
Xenophmix <xenophmix@gmail.com>
2022-11-28 16:02:22 +0800

    修改新聞顯示介面，熱門顯示HOT!!!!!，置頂的顯示TOP!!!

--- front/main.php
+++ front/main.php
@@ -1,19 +1,33 @@
 <h1 class="text-center">最新消息</h1>
-<ul class="list-group">
-<?php
-$all_news="SELECT * FROM `news` ";
-$rows=$pdo->query($all_news)->fetchAll();
+<main class="container">
 
-foreach($rows as $row){
-    echo "<div class='container'>";
-    echo "<li class='list-group-item list-group-item-action d-flex'>";
-    echo "<a class='text-primary' href='index.php?do=news_detail&id={$row['id']}'>";
-    echo $row['subject'];
-    echo "</a>";
-    echo "</li>";
-    echo "</div>";
-}
+  <ul class="list-group">
+    <li class='list-group-item list-group-item-action d-flex text-center bg-info text-white'>
+      <div class='col-md-10'>標題</div>
+      <div class='col-md-2'>人氣</div>
+    </li>
+    <?php
+    $all_news = "SELECT * FROM `news` ORDER BY `top` desc,`readed` desc ";
+    $rows = $pdo->query($all_news)->fetchAll();
+    $hot = $pdo->query("SELECT `id` FROM `news` ORDER BY `readed` desc")->fetchColumn();
 
-?>
+    foreach ($rows as $row) {
+      echo "<li class='list-group-item list-group-item-action d-flex'>";
+      echo "<div class='col-md-10 text-center'>";
+      echo ($row['top']==1)?"top!!!":'';
+      echo ($row['id']==$hot)?"hot!!!":'';
+      echo "<a class='text-dark' href='index.php?do=news_detail&id={$row['id']}'>";
+      echo $row['subject'];
+      echo "</a>";
+      echo "</div>";
+      echo "<div class='col-md-2 text-center'>";
+      echo $row['readed'];
+      echo "</div>";
+      echo "</li>";
+    }
 
-</ul>
+    ?>
+
+  </ul>
+
+</main>
