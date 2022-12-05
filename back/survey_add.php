79c5d71905bede7433d694c79f406c522c93d340
Xenophmix <xenophmix@gmail.com>
2022-12-05 10:47:47 +0800

    添加新問卷至資料庫

--- back/survey_add.php
+++ back/survey_add.php
@@ -1,31 +1,63 @@
 <h3 class="text-center text-white">新增調查</h3>
 
 <form action="./api/survey_add.php" method="post" class="col-10 mx-auto d-flex flex-wrap justify-content-end">
-    <div class="form-group row col-12">
+    <div class="form-group row col-12 gap-3">
       <div class="col-2 text-end">
         <label class="text-end text-white">主題</label>
       </div>
       <div class="col-8 text-end">
-        <input type="text" class="form-control">
+        <input name="subject" type="text" class="form-control">
       </div>
     </div> 
     <!--選項區-->
-        <div class="form-group row col-12 justify-content-center">
+        <div class="form-group row col-12 justify-content-center mt-2">
           <div class="col-1 text-end">
-            <label class="text-end text-white">項目1</label>
+            <label class="text-end text-white" >項目1</label>
           </div>
           <div class="col-7">
-            <input type="text" class="form-control">
+            <input type="text" class="form-control" name="opt[]">
           </div>
         </div>    
-        <div class="form-group row col-12 justify-content-center">
+        <div class="form-group row col-12 justify-content-center mt-2">
           <div class="col-1 text-end">
             <label class="text-end text-white">項目2</label>
           </div>
           <div class="col-7">
-            <input type="text" class="form-control">
+            <input type="text" class="form-control" name="opt[]">
           </div>
-        </div>      
+        </div>
+        <div class="form-group row col-12 justify-content-center mt-2">
+          <div class="col-1 text-end">
+            <label class="text-end text-white">項目3</label>
+          </div>
+          <div class="col-7">
+            <input type="text" class="form-control" name="opt[]">
+          </div>
+        </div>  
+        <div class="form-group row col-12 justify-content-center mt-2">
+          <div class="col-1 text-end">
+            <label class="text-end text-white">項目4</label>
+          </div>
+          <div class="col-7">
+            <input type="text" class="form-control" name="opt[]">
+          </div>
+        </div>
+        <div class="form-group row col-12 justify-content-center mt-2">
+          <div class="col-1 text-end">
+            <label class="text-end text-white">項目5</label>
+          </div>
+          <div class="col-7">
+            <input type="text" class="form-control" name="opt[]">
+          </div>
+        </div>    
+        <div class="form-group row col-12 justify-content-center mt-2">
+          <div class="col-1 text-end">
+            <label class="text-end text-white">項目6</label>
+          </div>
+          <div class="col-7">
+            <input type="text" class="form-control" name="opt[]">
+          </div>
+        </div>          
 <div class="text-center col-12 mt-3">
     <input class="btn btn-primary mx-1" type="submit" value="確定新增">
     <input class="btn btn-warning mx-1" type="reset" value="重置">
--- front/survey_item.php
+++ front/survey_item.php
@@ -1,28 +1,23 @@
 <h3 class="text-center font-weight-bold">Lorem ipsum dolor sit</h3>
 
 <form action="./api/survey_vote.php">
-  <div class="col-8 mx-auto mt-4">
-    <div class="input-group" style="margin-top:-1px">
-      <div class="input-group-prepend">
-        <div class="input-group-text">
-          <input type="radio" name="option">
-        </div>
+  <div class="container">
+
+    <div class="input-group mb-3">
+      <div class="input-group-text">
+        <input class="form-check-input mt-0" name="Multiple-choice" type="radio" value="" aria-label="Checkbox for following text input">
       </div>
-      <div class="form-control">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      <div class="form-control">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet repellendus voluptatem in? Amet, iusto rem. Incidunt nihil laboriosam rerum similique quidem vero fuga quod, rem ducimus blanditiis minima aut recusandae?
       </div>
     </div>
-    <div class="input-group" style="margin-top:-1px">
-      <div class="input-group-prepend">
-        <div class="input-group-text">
-          <input type="radio" name="option">
-        </div>
+    
+    <div class="input-group mb-3">
+      <div class="input-group-text">
+        <input class="form-check-input mt-0" name="Multiple-choice" type="radio" value="" aria-label="Checkbox for following text input">
       </div>
-      <div class="form-control">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      <div class="form-control">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore tempora, molestias ipsum voluptate deleniti alias commodi non minus quia dicta nostrum modi qui aut maxime quos esse voluptatibus enim debitis.
       </div>
     </div>
-  </div>
   <div class="text-center mt-4">
     <input type="submit" class="btn btn-primary mx-1" value="投票">
     <a href="index.php?do=survey" class="btn btn-warning mx-1">取消返回</a>
